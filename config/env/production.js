14e7563f60bb04858d6a4855fdf614b94c9200f6
Timothee Emery <tim@youshould.eu>
2017-02-19 17:51:16 +0100

    Dynamic app url
    Can be configure with env vars.

--- config/env/production.js
+++ config/env/production.js
@@ -9,26 +9,26 @@ module.exports = {
   facebook: {
     clientID: process.env.FACEBOOK_CLIENTID,
     clientSecret: process.env.FACEBOOK_SECRET,
-    callbackURL: 'http://nodejs-express-demo.herokuapp.com/auth/facebook/callback'
+    callbackURL: process.env.APP_URL + '/auth/facebook/callback'
   },
   twitter: {
     clientID: process.env.TWITTER_CLIENTID,
     clientSecret: process.env.TWITTER_SECRET,
-    callbackURL: 'http://nodejs-express-demo.herokuapp.com/auth/twitter/callback'
+    callbackURL: process.env.APP_URL + '/auth/twitter/callback'
   },
   github: {
     clientID: process.env.GITHUB_CLIENTID,
     clientSecret: process.env.GITHUB_SECRET,
-    callbackURL: 'http://nodejs-express-demo.herokuapp.com/auth/github/callback'
+    callbackURL: process.env.APP_URL + '/auth/github/callback'
   },
   linkedin: {
     clientID: process.env.LINKEDIN_CLIENTID,
     clientSecret: process.env.LINKEDIN_SECRET,
-    callbackURL: 'http://nodejs-express-demo.herokuapp.com/auth/linkedin/callback'
+    callbackURL: process.env.APP_URL + '/auth/linkedin/callback'
   },
   google: {
     clientID: process.env.GOOGLE_CLIENTID,
     clientSecret: process.env.GOOGLE_SECRET,
-    callbackURL: 'http://nodejs-express-demo.herokuapp.com/auth/google/callback'
+    callbackURL: process.env.APP_URL + '/auth/google/callback'
   }
 };
